bafb9af208c95259813b2acc33ab479392ae8e84
Neil <vitalene@msu.edu>
2016-10-04 16:29:41 -0400

    Initial Commit

---
+++ MyAccounts/MyAccounts.h
@@ -0,0 +1,19 @@
+//
+//  MyAccounts.h
+//  MyAccounts
+//
+//  Created by Neil Vitale on 10/4/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyAccounts.
+FOUNDATION_EXPORT double MyAccountsVersionNumber;
+
+//! Project version string for MyAccounts.
+FOUNDATION_EXPORT const unsigned char MyAccountsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAccounts/PublicHeader.h>
+
+

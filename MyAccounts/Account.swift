c88ef3f23b99aff3b7cb0daceaaa558974ca989e
Neil <vitalene@msu.edu>
2016-10-10 13:36:54 -0400

    updated names

--- MyAccounts/Account.swift
+++ MyAccounts/Account.swift
@@ -9,26 +9,26 @@ class Account {
     // Assets: Assets = Liabilities + Owners' Equity
     //
     // consider an enum here...
-    enum Category {
+    enum AccountCategory {
         case asset(isEquity: Bool)
         case liability
         case expense
         case revenue
     }
     //  let register = Ledger.init(with: [Transaction], with: [String], and: Float)
-    var category: Category
+    var category: AccountCategory
     var entries = [Transaction]()
     let columnTitles: [String]
     let currentBalance: NSDecimalNumber
     var runningTotal: NSDecimalNumber
     
     
-    init(entries: [Transaction], titles: [String], balance: NSDecimalNumber, category: Category) {
+    init(entries: [Transaction], titles: [String], initialBalance: NSDecimalNumber, category: AccountCategory) {
         self.entries = entries
         self.columnTitles = titles
-        self.currentBalance = balance
+        self.currentBalance = initialBalance
         self.category = category
-        self.runningTotal = balance
+        self.runningTotal = initialBalance
     }
     
     func getNewTotal(latestTransaction: Transaction, latestTransAmount: NSDecimalNumber, lastTotal: NSDecimalNumber) -> NSDecimalNumber {
--- MyAccounts/Transaction.swift
+++ MyAccounts/Transaction.swift
@@ -7,20 +7,25 @@ enum TransactionType {
     case debit
     case credit
 }
+enum currencyType {
+    case dollar
+}
+
 class Transaction {
     
     var date: Date
     var userProvidedDescription: String
     var amount: [String:NSDecimalNumber]
     var transactionType: TransactionType
-    
+    var currencyType: currencyType
 
     
-    init(on date: Date, description: String, for amount: [String:NSDecimalNumber], lastAccountTotal: NSDecimalNumber, type: TransactionType) {
+    init(on date: Date, description: String, for amount: [String:NSDecimalNumber], lastAccountTotal: NSDecimalNumber, type: TransactionType, currencyType: currencyType) {
         self.date = date
         self.userProvidedDescription = description
         self.amount = amount
         self.transactionType = type
+        self.currencyType = currencyType
         
     }
     
